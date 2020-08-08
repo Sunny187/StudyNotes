6dc89e258d28d0ba8d972e915e811db55fe05225
Sunny <2506095050@qq.com>
2020-08-08 22:34:57 +0800

    专门处理前端的js

---
+++ js/hello.js
@@ -0,0 +1,47 @@
+// document.write ("<h1>吴志敏</h1>") ;
+// document.getElementById("pid").innerHTML = "你一定可以成为你想要的那个人的" ;
+
+// function demo () {
+//     alert( "吴志敏是最帅的") ;
+// }
+
+
+// function demo () {
+//     try {
+//         alert (str) ;
+//     } catch (err) {
+//         alert (err) ;
+//     }
+
+// }
+
+// demo () ;
+
+
+// function demo () {
+//     let str = document.getElementById("txt").value ;
+//     try {
+//         if (str == "") {
+//             throw "用户输入异常请输入" ;
+//         }
+//     } catch (err) {
+//         alert (err);
+//     }
+// }
+
+
+// function onOver (ooj) {
+//     console.log(ooj) ;
+//     ooj.innerHTML = "hello" ;
+// }
+
+
+// function onOut (ooj) {
+//     console.log(ooj) ;
+//     ooj.innerHTML = "world" ;
+// }
+
+
+// function changeDemo (bg) {
+//     alert("文本发生变化");
+// }
