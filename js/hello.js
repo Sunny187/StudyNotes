23d47de33a7df6327800cec0a5226fc5ce750a12
Sunny <2506095050@qq.com>
2020-08-09 11:01:56 +0800

    js 山腰进度

--- js/hello.js
+++ js/hello.js
@@ -45,3 +45,124 @@
 // function changeDemo (bg) {
 //     alert("文本发生变化");
 // }
+
+
+// function demo (){
+//     document.write("world");
+// }
+
+// function demo () {
+//     document.getElementById("pid").innerHTML="好好加油" ;
+// }
+
+
+// function demo () {
+//     document.getElementById("aid").href="#" ;
+// }
+
+
+// function demo () {
+//     document.getElementById("did").style.backgroundColor= "red" ;
+// }
+
+
+// document.getElementById("btn").addEventListener("click" , function(){
+//     alert ("haoha") ;
+// });
+
+
+// document.getElementById("btn").addEventListener("click" , function(){
+//     alert ("hello") ;
+// });
+
+// document.getElementById("btn").addEventListener("click" , function(){
+//     alert ("world") ;
+// });
+
+
+// document.getElementById("btn").addEventListener("click" , demo);
+// document.getElementById("did").addEventListener("click" , demo1);
+
+
+// function demo(event){
+//     // alert (event.type) ;
+//     alert(event.target);
+//     event.stopPropagation();//阻止事件冒泡
+    
+// }
+// function demo1(event){
+//     // alert (event.type) ;
+//     alert("div") ;
+// }
+
+//访问子节点
+// function getChildNode () {
+//     var  childnode = document.getElementsByTagName ("ul")[0].childNodes ;
+//     alert(childnode.length);
+// }
+// getChildNode ();
+
+// //访问父节点
+// function getParent () {
+//     let a = document.getElementById("pid").parentNode ;
+//     alert (a.nodeName) ;
+// }
+
+// getParent ();
+
+// function createNodes () {
+//     let body = document.body ;    
+//     let input = document.createElement("input") ;
+//     input.type = "button" ;
+//     input.value = "按钮" ;
+//     body.appendChild (input) ;
+//  }
+// createNodes () ;
+
+ 
+// function addNode () {
+//     let body = document.body ;
+//     let div = document.getElementById ("div") ;
+//     let node = document.getElementById ("pid") ;
+//     let newNode = document.createElement ("p") ;
+//     newNode.innerHTML = "动态添加一个元素" ;
+//     body.insertBefore(newNode , div) ;
+// }
+// addNode () ;
+
+
+//document.write ("宽度 : " , window.innerWidth , " 高度 : " , window.innerHeight) ;
+
+// function btnClick () {
+//     window.open ("new.html") ;
+// }
+
+var myTime = setInterval (function () {
+    getTime () ;
+} , 1000) ;
+
+function getTime () {
+    var  d = new Date () ;
+    var  t = d.toLocaleTimeString();
+    document.getElementById ("pid").innerHTML = t ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
